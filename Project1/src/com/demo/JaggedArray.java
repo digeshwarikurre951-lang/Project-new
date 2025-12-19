d000795c4dcbfbe676ead64ca3685a6fb852aa4f
Digeshwari Kurre <digeshwarikurre037@gmail.com>
2025-12-19 22:01:52 +0530

    Project

--- Project1/src/com/demo/JaggedArray.java
+++ Project1/src/com/demo/JaggedArray.java
@@ -2,28 +2,23 @@ package com.demo;
 
 import java.util.*;
 
-public class Array2{
+public class JaggedArray{
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		int a[][] = new int[3][];
 		
-//		
-	    System.out.print("Enter no. of class : ");
-		int m = sc.nextInt();
-	    System.out.print("Enter no. of Students : ");
-		int n = sc.nextInt();
-		int a[][] = new int[m][n];
-		
+		for(int i=0;i<a.length;i++) {
+			System.out.println("Enter the total no.of students in class" + i);
+			a[i] = new int[sc.nextInt()];
+		}
+
 		for(int i=0; i<a.length; i++) {
 			for(int j=0; j<a[i].length; j++) {       
 			  System.out.print("Please enter the marks of class " + i + " student " + j + " ");
 				a[i][j] = sc.nextInt();
 			}
 		}
-		
-		System.out.println();
-		System.out.println("All marks in 2D Array");
-		System.out.println();
 		
 		for(int i=0;i<a.length;i++) {
 			for(int j=0;j<a[i].length;j++) {
